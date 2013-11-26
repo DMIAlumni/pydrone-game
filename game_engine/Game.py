4070f3ad4695af47ae43738e43d3d10096dedbbd
Federico Dolce <psykopear@gmail.com>
2013-11-26 17:58:01 +0100

    Turn based game engine

--- game_engine/Game.py
+++ game_engine/Game.py
@@ -2,10 +2,22 @@ class Game(object):
     def __init__(self, world, drones):
         self.world = world
         self.drones = drones
+        self.asset_not_found = True
 
     def start_game(self):
-        for drone in self.drones:
+        while(self.asset_not_found):
+            drone = self.next_drone()
             drone.probe(self.world[drone.actual_position[0]][drone.actual_position[1]])
-            drone.print_world()
+            x, y = drone.strategy()
+            drone.move(x, y)
+            if self.asset_found(x, y):
+                self.asset_not_found = False
+        print "Found!"
 
-        return
+    def next_drone(self):
+        ndrone = self.drones.pop()
+        self.drones.append(ndrone)
+        return ndrone
+
+    def asset_found(self, x, y):
+        return self.world[x][y] == -1
