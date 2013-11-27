4b5e21fcc4608b05f893f85d97fd08a1462e6d03
Federico Dolce <psykopear@gmail.com>
2013-11-27 16:08:55 +0100

    World parameters from ARGV

--- drone_game.py
+++ drone_game.py
@@ -1,17 +1,22 @@
+from sys import argv
+
 from game_engine import Game, Drone
 from utils.matrix_generator import world_generator
 
 # Global parameters
-MATRIX_SIZE = 40
-END_X = 36
-END_Y = 32
+script, size, x, y, drone_x, drone_y = argv
+
+MATRIX_SIZE = int(size)
+END_X = int(x)
+END_Y = int(y)
+START_X = int(drone_x)
+START_Y = int(drone_y)
 
 
 # Algorithm parameters
 # ...
 
 world = world_generator(MATRIX_SIZE, END_X, END_Y)
-drones = [Drone(MATRIX_SIZE, 2, 5)]
+drones = [Drone(MATRIX_SIZE, START_X, START_Y)]
 game = Game(world, drones)
 game.start_game()
-#print_matrix(game.world)
