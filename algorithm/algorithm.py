81de18b193b9747d04f80fd6b3cecaf655578e01
Federico Dolce <psykopear@gmail.com>
2013-11-27 16:28:58 +0100

    Refactoring algorythm.py code

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -2,30 +2,16 @@ from utils.direction_modifier import modifier
 
 
 def search_far(kb, actual_position, distances, drone):
-    # Variabili
-
-    BORDER_X_R = False
-    BORDER_X_L = False
-    BORDER_Y_U = False
-    BORDER_Y_D = False
-    GOING_FAR = False
-    LIMIT = (len(kb[0]) - 1)
     X = actual_position[0]
     Y = actual_position[1]
     STEP = len(distances)
+    LIMIT = len(kb[0]) - 1
 
     # Controllo dell'actual_position per sapere se si e' sul bordo
-
-    if X == LIMIT:
-        BORDER_X_R = True
-    if X <= 0:
-        BORDER_X_L = True
-    if Y == LIMIT:
-        BORDER_Y_D = True
-    if Y <= 0:
-        BORDER_Y_U = True
-
-    # Algoritmo vero e proprio
+    BORDER_X_R = X == LIMIT
+    BORDER_Y_U = Y == LIMIT
+    BORDER_X_L = X <= 0
+    BORDER_Y_D = Y <= 0
 
     if STEP == 1 or STEP == 2:
         direction = 0
@@ -34,39 +20,40 @@ def search_far(kb, actual_position, distances, drone):
     elif STEP == 5:
         direction = 5
     else:
+        # Le tre misurazioni della "triangolazione" per capire
+        # il quadrante in cui si trova il punto d'arrivo
+        first_measure = distances[0]
+        second_measure = distances[2]
+        third_measure = distances[4]
 
-        if distances[-1] > distances[-2]:
-            GOING_FAR = True
-
-        dist1 = distances[0]
-        dist2 = distances[2]
-        dist3 = distances[4]
-# E se invece di muovermi in una direzione a caso nel quadrante in cui e' supposto essere l'arrivo
-# togliessi dal kb tutta quella parte di mondo dove sicuramente non devo andare?
-        if dist1 > dist2:
-            if dist2 < dist3:
+        if first_measure > second_measure:
+            if second_measure < third_measure:
                 direction = 7
-            elif dist2 > dist3:
+            elif second_measure > third_measure:
                 direction = 1
             else:
                 direction = 0
-        elif dist1 < dist2:
-            if dist2 < dist3:
+        elif first_measure < second_measure:
+            if second_measure < third_measure:
                 direction = 5
-            elif dist2 > dist3:
+            elif second_measure > third_measure:
                 direction = 3
             else:
                 direction = 4
         else:
-            if dist2 < dist3:
+            if second_measure < third_measure:
                 direction = 6
-            elif dist2 > dist3:
+            elif second_measure > third_measure:
                 direction = 2
-    if GOING_FAR:
+
+    if distances[-1] > distances[-2]:
         drone.distances = []
 
     modifier_x = modifier(direction)[0]
     modifier_y = modifier(direction)[1]
+
+    # Cambio del verso del movimento se raggiungo il bordo
+    # ad esempio bordo destro, mando x in direzione -1, sinistra
     if BORDER_X_R:
         modifier_x = -1
     if BORDER_X_L:
