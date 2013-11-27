466fd2e1838716be5947ef46b7a047b61f2b4af0
Federico Dolce <psykopear@gmail.com>
2013-11-27 12:40:22 +0100

    Better direction modifier

--- utils/direction_modifier.py
+++ utils/direction_modifier.py
@@ -1,21 +1,18 @@
-def modifier(direction):
-#{ 0 : (1,0), 1 : (1,-1), 2 : (0,-1), 3 : (-1,-1), 4 : (-1,0), 5 : (-1,1), 6 : (0,1), 7 : (1,1) }
-    if direction == 0:
-        return (1, 0)
-    elif direction == 1:
-        return (1, -1)
-    elif direction == 2:
-        return (0, -1)
-    elif direction == 3:
-        return (-1, -1)
-    elif direction == 4:
-        return (-1, 0)
-    elif direction == 5:
-        return (-1, 1)
-    elif direction == 6:
-        return (0, 1)
-    elif direction == 7:
-        return (1, 1)
+directions = {
+    0 : (1, 0),
+    1 : (1, -1),
+    2 : (0, -1),
+    3 : (-1, -1),
+    4 : (-1, 0),
+    5 : (-1, 1),
+    6 : (0, 1),
+    7 : (1, 1),
+}
+
+
+def modifier(way):
+    return directions.get(way)
+
 
 def invert_direction(direction):
     return (direction + 4)  % 8
