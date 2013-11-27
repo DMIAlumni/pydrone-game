af02ac03e53f32dd911de582639a59febe659536
Federico Dolce <psykopear@gmail.com>
2013-11-27 16:00:44 +0100

    Avoid algorithm crash on border. Fixed #1

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -1,29 +1,48 @@
 from utils.direction_modifier import modifier
-from utils.direction_modifier import invert_direction
 
 
 def search_far(kb, actual_position, distances, drone):
-    # Controllo dell'actual_position per sapere se si e' sul bordo
-    BORDER = False
+    # Variabili
+
+    BORDER_X_R = False
+    BORDER_X_L = False
+    BORDER_Y_U = False
+    BORDER_Y_D = False
     GOING_FAR = False
+    LIMIT = (len(kb[0]) - 1)
     X = actual_position[0]
     Y = actual_position[1]
-    LIMIT = (len(kb[0]) - 1)
-    if X == LIMIT or Y == LIMIT or X <= 0 or Y <= 0 :
-        BORDER = True
-    step = len(distances)
-    if step == 1 or step == 2:
+    STEP = len(distances)
+
+    # Controllo dell'actual_position per sapere se si e' sul bordo
+
+    if X == LIMIT:
+        BORDER_X_R = True
+    if X <= 0:
+        BORDER_X_L = True
+    if Y == LIMIT:
+        BORDER_Y_D = True
+    if Y <= 0:
+        BORDER_Y_U = True
+
+    # Algoritmo vero e proprio
+
+    if STEP == 1 or STEP == 2:
         direction = 0
-    elif step == 3 or step == 4:
+    elif STEP == 3 or STEP == 4:
         direction = 2
-    elif step == 5:
+    elif STEP == 5:
         direction = 5
     else:
+
         if distances[-1] > distances[-2]:
             GOING_FAR = True
+
         dist1 = distances[0]
         dist2 = distances[2]
         dist3 = distances[4]
+# E se invece di muovermi in una direzione a caso nel quadrante in cui e' supposto essere l'arrivo
+# togliessi dal kb tutta quella parte di mondo dove sicuramente non devo andare?
         if dist1 > dist2:
             if dist2 < dist3:
                 direction = 7
@@ -43,8 +62,18 @@ def search_far(kb, actual_position, distances, drone):
                 direction = 6
             elif dist2 > dist3:
                 direction = 2
-    if GOING_FAR == True:
+    if GOING_FAR:
         drone.distances = []
-    if BORDER == True:
-        direction = invert_direction(direction)
-    return X + modifier(direction)[0], Y + modifier(direction)[1]
+
+    modifier_x = modifier(direction)[0]
+    modifier_y = modifier(direction)[1]
+    if BORDER_X_R:
+        modifier_x = -1
+    if BORDER_X_L:
+        modifier_x = 1
+    if BORDER_Y_U:
+        modifier_y = 1
+    if BORDER_Y_D:
+        modifier_y = -1
+
+    return X + modifier_x, Y + modifier_y
--- utils/direction_modifier.py
+++ utils/direction_modifier.py
@@ -1,18 +1,14 @@
 directions = {
-    0 : (1, 0),
-    1 : (1, -1),
-    2 : (0, -1),
-    3 : (-1, -1),
-    4 : (-1, 0),
-    5 : (-1, 1),
-    6 : (0, 1),
-    7 : (1, 1),
+    0: (1, 0),
+    1: (1, -1),
+    2: (0, -1),
+    3: (-1, -1),
+    4: (-1, 0),
+    5: (-1, 1),
+    6: (0, 1),
+    7: (1, 1),
 }
 
 
 def modifier(way):
     return directions.get(way)
-
-
-def invert_direction(direction):
-    return (direction + 4)  % 8
