51c7cfcebbfc857dca42e15b72654015a30e7e1f
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-05-31 12:08:29 +0200

    pydrone with click interface

--- pydrone.py
+++ pydrone.py
@@ -1,4 +1,4 @@
-from sys import argv
+import click
 
 from pydrone.game import SingleAnchorSearchGame
 from pydrone.utils.matrix_generator import world_generator
@@ -6,17 +6,17 @@ from pydrone.utils.matrix_generator import world_generator
 from pydrone.drones.generic import GreedyCompleteDrone
 from pydrone.contrib.drones import GeometricDrone
 
-# Global parameters
 
-
-# Algorithm parameters
-# ...
-
-
-def main(size, x, y, drone_x, drone_y, knowledge=False):
+@click.command()
+@click.option('--size', default=40, help='Matrix size')
+@click.argument('anchor-x', default=5)
+@click.argument('anchor-y', default=19)
+@click.argument('drone-x', default=30)
+@click.argument('drone-y', default=30)
+def single_anchor(size, anchor_x, anchor_y, drone_x, drone_y, knowledge=False):
     MATRIX_SIZE = int(size)
-    END_X = int(x)
-    END_Y = int(y)
+    END_X = int(anchor_x)
+    END_Y = int(anchor_y)
     START_X = int(drone_x)
     START_Y = int(drone_y)
     KNOWLEDGE = knowledge
@@ -29,6 +29,6 @@ def main(size, x, y, drone_x, drone_y, knowledge=False):
     moves = game.start_game()
     print "Total moves: ", moves
 
+
 if __name__ == "__main__":
-    script, size, x, y, drone_x, drone_y = argv
-    main(size, x, y, drone_x, drone_y)
+    single_anchor()
