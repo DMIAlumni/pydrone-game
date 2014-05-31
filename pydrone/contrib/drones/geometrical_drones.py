7b8002552d9a46a489eb6630a55f51a02fb0ea5d
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-05-31 11:30:04 +0200

    Geometric drone class

---
+++ pydrone/contrib/drones/geometrical_drones.py
@@ -0,0 +1,36 @@
+from pydrone.drones import BaseDrone
+from pydrone.drones.mixins import DroneGraph
+from pydrone.utils.matrix_generator import print_matrix
+
+from pydrone.contrib.algorithms.geometrical import GeometricAlgorithm
+
+
+class GeometricDrone(DroneGraph, BaseDrone):
+    """
+    Federico Dolce GeometricDrone
+    """
+
+    # TODO: create a mixin to choose drone algorithm (better drone interface)
+    algorithm = GeometricAlgorithm()
+
+    def move(self, x, y):
+        self.fuel -= 1
+        self.actual_position = (x, y)
+        self.graph[x][y] += 1
+        self.kb.add_node_coord((x, y))
+
+    def strategy(self):
+        x, y = self.algorithm.search_far_calibration(self)
+
+        # This parameter has a great influence on algorithm quality
+        if self.graph[x][y] <= 1:
+            return x, y
+        else:
+            return self.algorithm.change_strategy(self)
+
+    def print_world(self):
+        print_matrix(self.graph)
+
+    def probe(self, distance):
+        self.distances.append(distance)
+        self.kb.change_weight(self.actual_position, distance)
--- pydrone/drones/base.py
+++ pydrone/drones/base.py
@@ -3,27 +3,31 @@ class BaseDrone(object):
     Drone interface
     """
 
-    def __init__(self, world_size, x, y):
-        self.flipflop = False
-        self.last_modifier = 0
-        self.actual_position = (x, y)
-        self.fuel = 2000
-        self.distances = []
-        self.last_direction = 0
+    flipflop = False
+    distances = []
+    last_direction = 0
+    last_modifier = 0
+    fuel = 2000
 
-        # Initialize knoledge base
+    graph = None
+    kb = None
+    algorithm = None
+
+    def __init__(self, world_size, x, y):
+        # Initialize knowledge base and starting position
+        self.actual_position = (x, y)
         self.kb_generator(world_size, x, y)
 
     def kb_generator(self, world_size, x, y):
         raise NotImplementedError()
 
-    def move(self):
+    def move(self, x, y):
         raise NotImplementedError()
 
     def strategy(self):
         raise NotImplementedError()
 
-    def probe(self):
+    def probe(self, distance):
         raise NotImplementedError()
 
     def print_status(self):
